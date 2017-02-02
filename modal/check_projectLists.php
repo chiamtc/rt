170942fd1839bd4cb477998a7f1ab6441bb041b0
DESKTOP-VJNO5T1\chiam <chiamcheng@gmail.com>
2017-02-02 18:37:23 +1000

    delete previous project and starting with angularjs and php

--- modal/check_projectLists.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "db_connection.php";
-
-$sql_checkProject = "select * from p.project inner join up.userproject on up.userproject = 1";
-$result = $conn -> query($sql_checkProject);
-$response = array();
-
-?>
